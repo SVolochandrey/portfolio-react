2d3e945a269eccd3c49ee4f779c42fb086907d08
SVolochandrey <volochan.andrey@gmail.com>
2023-06-10 17:49:46 +0300

    +

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import ScrollToTop from "./utils/scrollToTop"
 function App() {
   return (
 		<div className="App">
-			<Router>
+			<Router basename="/portfolio-react">
 				<ScrollToTop />
 				<Navbar />
 				<Routes>
@@ -30,3 +30,38 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import Navbar from './components/navbar/Navbar';
+// import Footer from './components/footer/Footer';
+// import Home from './pages/Home';
+// import Projects from './pages/Projects';
+// import Project from './pages/Project';
+// import Contacts from './pages/Contacts';
+
+// import ScrollToTop from './utils/scrollToTop';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router basename="/portfolio-react">
+//         <ScrollToTop />
+//         <Navbar />
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/projects" element={<Projects />} />
+//           <Route path="/project/:id" element={<Project />} />
+//           <Route path="/contacts" element={<Contacts />} />
+//         </Routes>
+//         <Footer />
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
