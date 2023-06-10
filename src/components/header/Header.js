630a0e9fcdfd3b2bc1deebc40003d316c66769b5
SVolochandrey <volochan.andrey@gmail.com>
2023-06-10 20:34:30 +0300

    +

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,31 @@
-import "./style.css";
+import './style.css';
 import avatar from './avatar.jpg';
 
 const Header = () => {
-    return (
-		<header className="header">
-			<div className="header__wrapper">
-				<img src={avatar} alt="avatar" className="header__avatar" />
-				<h1 className="header__title">
-					<strong>
-						Hi, i'm <em>Andrew</em>
-					</strong>
-					<br />junior front-end developer
-				</h1>
-				<div className="header__text">
-					<p>with passion for learning and creating.</p>
-				</div>
-			</div>
-		</header>
-	);
-}
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <img src={avatar} alt="avatar" className="header__avatar" />
+        <h1 className="header__title">
+          <strong>
+            Hi, i'm <em>Andrew</em>
+          </strong>
+          <br />
+          junior front-end developer
+        </h1>
+        <div className="header__text">
+          <p>
+            I am a front-end developer with good knowledge in HTML, CSS, and
+            JavaScript. I strive to create websites with appealing designs and
+            user-friendly experiences. I am eager to improve my skills in React
+            and learn efficient state management and building reusable
+            components. I am ready to actively learn and join a team where I can
+            contribute and grow alongside experienced developers.
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,32 +1,40 @@
-const Contacts = () => {
-    return (
-		<main className="section">
-			<div className="container">
-				<h1 className="title-1">Contacts</h1>
+import { TiLocation } from 'react-icons/ti';
 
-				<ul className="content-list">
-					<li className="content-list__item">
-						<h2 className="title-2">Location</h2>
-						<p>Odesa, Ukraine</p>
-					</li>
-					<li className="content-list__item">
-						<h2 className="title-2">Telegram / WhatsApp</h2>
-						<p>
-							<a href="tel:+79051234567">+380 (96) 855-08-08</a>
-						</p>
-					</li>
-					<li className="content-list__item">
-						<h2 className="title-2">Email</h2>
-						<p>
-							<a href="mailto:webdev@protonmail.com">
-								volochan.andrey@gmail.com
-							</a>
-						</p>
-					</li>
-				</ul>
-			</div>
-		</main>
-	);
-}
+const Contacts = () => {
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title-1">Contacts</h1>
+
+        <ul className="content-list">
+          <li className="content-list__item">
+            <h2 className="title-2">Location</h2>
+            <a
+              href="https://goo.gl/maps/YENSrpa8iaBnQCPn7"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <TiLocation className="locationIcon" /> Odesa, Ukraine
+            </a>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Telegram / WhatsApp</h2>
+            <p>
+              <a href="tel:+79051234567">+380 (96) 855-08-08</a>
+            </p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Email</h2>
+            <p>
+              <a href="mailto:webdev@protonmail.com">
+                volochan.andrey@gmail.com
+              </a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
 
 export default Contacts;
