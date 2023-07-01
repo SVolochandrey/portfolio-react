b5d0530c71179eb50edaaa4be56fb23c8f4cdab2
SVolochandrey <volochan.andrey@gmail.com>
2023-07-01 14:55:38 +0300

    adding adaptive for slider

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -162,14 +162,14 @@ const Projects = () => {
         breakpoint: 1200,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
+          slidesToScroll: 2,
         },
       },
       {
         breakpoint: 1400,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1,
+          slidesToScroll: 3,
         },
       },
     ],
